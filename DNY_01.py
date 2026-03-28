35a48ebdd6f98db8579a776aa95873cdb44a5fd2
hemantparmar-web <hemant.parmar@avantika.edu.in>
2026-03-28 11:18:56 +0000

    first

--- DNY_01.py
+++ DNY_01.py
@@ -1,11 +1,8 @@
 def calculate_fine(book_title, days_overdue, daily_rate=5.0, max_fine=150.0):
-    fine=days_overdue*daily_rate
-if(fine>=max_fine)
-print("You have accumulated the maximum fine of INR: 150.0")
-return fine
-book_title = input()
-days_overdue = int(input())
-fine = calculate_fine(book_title, days_overdue)
-print(f"Book: {book_title}")
-print(f"Days overdue: {days_overdue}")
-print(f"Fine: Rs. {fine}")
+    fine = days_overdue * daily_rate
+
+    if fine > max_fine:
+        fine = max_fine
+        print("You have accumulated the maximum fine of INR: 150.0")
+
+    return fine   
