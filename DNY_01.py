9a24cf3caf0c13e4ab82fafe0573b42ee9253b0e
hemantparmar-web <hemant.parmar@avantika.edu.in>
2026-03-28 10:56:45 +0000

    end

--- DNY_01.py
+++ DNY_01.py
@@ -6,3 +6,6 @@ return fine
 book_title = input()
 days_overdue = int(input())
 fine = calculate_fine(book_title, days_overdue)
+print(f"Book: {book_title}")
+print(f"Days overdue: {days_overdue}")
+print(f"Fine: Rs. {fine}")
