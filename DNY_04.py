69713741b6117de15f2e34284378ad9364034b38
hemantparmar-web <hemant.parmar@avantika.edu.in>
2026-03-28 16:28:16 +0000

    write

--- DNY_04.py
+++ DNY_04.py
@@ -1,3 +1,11 @@
 #test name = T4
 #ID = t4
+def calculate_fine(book_title, days_overdue, daily_rate, max_fine=150.0):
+    fine = days_overdue * daily_rate
+
+    if fine > max_fine:
+        fine = max_fine
+        print("You have accumulated the maximum fine of INR: 150.0")
+
+    return fine   
 
