9d8a5847e12d2d790da0c582b3d4d8c2249f8872
hemantparmar-web <hemant.parmar@avantika.edu.in>
2026-03-28 17:49:07 +0000

    execute

--- problem_01.py
+++ problem_01.py
@@ -8,3 +8,6 @@ def calculate_fine(book_title, days_overdue, daily_rate=5.00, max_fine=150.0):
 book_title = input()
 days_overdue = int(input())
 fine = calculate_fine(book_title, days_overdue)
+print("Book:", book_title)
+print("Days overdue:", days_overdue)
+print("Fine: Rs.", float(fine))
